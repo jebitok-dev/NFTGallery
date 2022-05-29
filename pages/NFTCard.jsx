397aaf8f6c7c74b4e725c7e142bf550d2028712f
Sharon Jebitok <sharonkosgei4@gmail.com>
2022-05-29 23:16:30 +0300

    Add Nextjs & API setup

---
+++ pages/NFTCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function NFTCard({nft}) {
+  return (
+    <div className="w-1/4 flex flex-col">
+        <div className="rounded-md">
+            <Image className="object-cover h-128 w-full rounded-t-md" src={NFTCard.media[0].gateway} />
+        </div>
+        <div className="flex flex-col y-gap-2 px-2 py-3 bg-slate-100 rounded-b-md h-110">
+            <div className="">
+                <h2 className="text-xl text-gray-800">{nft.title}</h2>
+                <p className="text-gray-600">Id: {nft.id.tokenId}</p>
+                <p className="text-gray-600">{`${nft.contract.address}`}</p>
+            </div>
+            <div className="flex-grow mt-2">
+                <p className="text-gray-600">{nft.description}</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
